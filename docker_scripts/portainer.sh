202acea63e5f6eb2523849b52e83d80eb9e87cca
Agustí Becerra Milà <contact@agustibm.com>
2017-04-05 15:09:55 +0200

    Fix

--- docker_scripts/portainer.sh
+++ docker_scripts/portainer.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -d -p 9000:9000 --privileged -v /var/run/docker.sock:/var/run/docker.sock portainer/portainer --no-auth
+docker run -d -p 9000:9000 --privileged -v /var/run/docker.sock:/var/run/docker.sock --name portainer portainer/portainer
